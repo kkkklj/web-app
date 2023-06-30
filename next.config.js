d38e36e7ff01af98d5bcfb3ca8f7af0d3d034031
kkkklj <819103524@qq.com>
2023-06-30 17:24:03 +0800

    webpack config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack');
 const nextConfig = {
+  webpack: (config) => {
+    // console.log(config.target);
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /.awebp$/,
+      })
+    );
+    return config;
+  },
 }
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
